db31ac8f45bebf24fec928fe5a2f552d1a20aaba
udayempire <kuday0726@gmail.com>
2024-12-26 09:22:12 +0530

    adding frontend and dashboard components

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,12 +1,11 @@
-"use client"
-import { signOut, signIn , useSession } from "next-auth/react"
-import { Appbar } from "@repo/ui/appbar"
-
-export default function Page():JSX.Element {
-  const session = useSession();
-  return (
-    <div>
-      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user}/>
-    </div>
-  )
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "./lib/auth";
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  if(session?.user){
+    redirect('/dashboard')
+  }else{
+    redirect('/api/auth/signin')
+  }
 }
