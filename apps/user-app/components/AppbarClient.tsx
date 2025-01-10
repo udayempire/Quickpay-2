edf5d3fbf717fbc08d605dfdd2d13fa313712405
udayempire <kuday0726@gmail.com>
2025-01-11 04:10:01 +0530

    fixed appbarCLuent

--- apps/user-app/components/AppbarClient.tsx
+++ apps/user-app/components/AppbarClient.tsx
@@ -2,8 +2,6 @@
 import { Appbar } from "@repo/ui/appbar"
 import { signIn, signOut, useSession } from "next-auth/react"
 import { useRouter } from "next/navigation"
-import { getServerSession } from "next-auth"
-import { authOptions } from "../app/lib/auth"
 export const AppbarClient=()=>{
     const router = useRouter();
     const session = useSession();
